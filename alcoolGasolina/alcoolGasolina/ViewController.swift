c2b11e0a150ca7971d55a20c4783a75794abe1b2
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-04-17 11:02:35 -0300

    finalização do app

--- alcoolGasolina/alcoolGasolina/ViewController.swift
+++ alcoolGasolina/alcoolGasolina/ViewController.swift
@@ -10,6 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var gasField: UITextField!
+    @IBOutlet weak var alcoolField: UITextField!
+    
+    @IBAction func calcularCombstivel(_ sender: Any) {
+        
+        if(self.validarCampos(precoAlcool: alcoolField.text!, precoGasolina: gasField.text!)){
+            self.calcularMelhorPreco(precoAlcool: alcoolField.text!, precoGasolina: gasField.text!)
+        }
+            
+        
+    }
+    
+    func calcularMelhorPreco(precoAlcool:String, precoGasolina:String) {
+        let valorAlcool:Double = Double(precoAlcool)!
+        let valorGasolina:Double = Double(precoGasolina)!
+        
+        let result = valorAlcool/valorGasolina
+        if(result >= 0.7){
+            resultLabel.text = "Melhor utilizar GASOLINA!"
+        }else {
+            resultLabel.text = "Melhor utilizar ALCOOL!"
+        }
+        
+    }
+    
+    func validarCampos(precoAlcool:String, precoGasolina:String) -> Bool {
+        var camposValidados:Bool = true
+        
+        if (precoAlcool.isEmpty)||(precoGasolina.isEmpty){
+            camposValidados = false
+            resultLabel.text = "Preencha todos os campos"
+        }
+        
+        return camposValidados
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
